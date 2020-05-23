b0dc07f265610cf50515cc8d337885883c39691e
Kyosuke <kyou0209@gmail.com>
2020-05-23 23:20:35 +0900

    [ADD]axiosの導入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 // Bootstrap
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -11,8 +15,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 // GoogleMap
 import * as VueGoogleMaps from 'vue2-google-maps'
 
+Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
-
 Vue.use(VueGoogleMaps, {
   load: {
     key: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
