f2c0edcf97fcbe5cd860ff39ddd9107f4bcc2f5e
Kyosuke <kyou0209@gmail.com>
2020-05-23 15:59:46 +0900

    [ADD]GoogleMapの表示

--- src/main.js
+++ src/main.js
@@ -2,12 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import BootstrapVue from 'bootstrap-vue'
 
+// Bootstrap
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// GoogleMap
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 Vue.use(BootstrapVue)
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyC1ZRrN9Wb4PU-0B-0DQhwD1IgVAFWk8uU',
+    libraries: 'places',
+    region: 'JP',
+    language: 'ja'
+  }
+})
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,97 @@
 <template>
-  <div class="home">
-    <h1>This is an home page</h1>
+  <div class="map-container">
+    <!--
+      :center     => 表示位置の中心を設定
+      :zoom       => 拡大率
+      :options    => オプションを指定
+      map-type-id => 表示するマップ種別
+      @xxxx       => 公式ドキュメントのイベントなら設定可能
+     -->
+    <GmapMap
+      class="google-map"
+      :center="{lat:35.690921, lng:139.70025799999996}"
+      :zoom="12"
+      :options="{streetViewControl: false}"
+      map-type-id="roadmap">
+
+      <!--
+        :options    => オプション設定
+        :position   => ウィンドウの表示位置 { lat: xxx, lng: xxx }
+        :opend      => 表示フラグ
+       -->
+      <GmapInfoWindow
+        v-for="m in markers"
+        :key=m.id
+        :options="infoOptions"
+        :position="m.position"
+        :opened="m.opend"
+        @closeclick="close(m)"
+      >
+      {{ m.info }}
+      </GmapInfoWindow>
+
+      <!--
+        :position   => マーカーの表示位置
+       -->
+      <GmapMarker
+        v-for="(m, idx) in markers"
+        :key="idx"
+        :position="m.position"
+        @click="open(m)" />
+
+    </GmapMap>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      // 取得したマーカーの一覧
+      markers: [
+        {
+          position: { lat: 35.690921, lng: 139.70025799999996 },
+          opend: false,
+          info: 'テスト'
+        },
+        {
+          position: { lat: 35.690921, lng: 139.80025799999996 },
+          opend: false,
+          info: 'test'
+        }
+      ],
+
+      // ウィンドウ表示時のオフセット設定
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35
+        }
+      }
+    }
+  },
+  methods: {
+    open (marker) {
+      marker.opend = true
+    },
+    close (marker) {
+      marker.opend = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.map-container {
+  width: 100vw;
+  height: 70vh;
+}
+
+.google-map {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
